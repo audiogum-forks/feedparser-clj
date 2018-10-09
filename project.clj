17122c20444f19807c8ed6f4334c02d75004794a
Andrew Jones <github@a4j.xyz>
2018-10-09 14:22:20 +0100

    Use 'audiogum' group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.gnzh/feedparser-clj "0.6.0"
+(defproject audiogum/feedparser-clj "0.6.1-SNAPSHOT"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.jdom/jdom2 "2.0.6"]
